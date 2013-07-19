64ba4d459bf9ac018066396523f028a5b990b7d8
Ubuntu <ubuntu@ip-172-31-33-56.us-west-2.compute.internal>
2013-07-19 04:44:41 +0000

    added responsive design

--- web.js
+++ web.js
@@ -9,7 +9,8 @@ app.get('/', function(request, response) {
   // response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
+console.log(port);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
